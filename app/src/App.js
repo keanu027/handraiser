dbe95f9d7e31657e944025cbd07ad3b988ac11d7
keanu manly <50285764+keanu027@users.noreply.github.com>
2019-08-13 13:29:10 +0800

    Revert "Feature/Dashboard backend"

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,25 @@
-import React, {useState} from 'react';
-import { Route, Switch, NavLink } from 'react-router-dom';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Login from './components/try'
 
 function App() {
-
-
   return (
-         <Switch>
-            <Route path='/api/login'
-              render={ props=>  <Login  {...props} /> } />
-         </Switch>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- app/src/components/try.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Login(){
-    return (
-        <span>Login </span>
-    )
-}
-
-export default Login;
